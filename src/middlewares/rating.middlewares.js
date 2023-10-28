55bbc0215d81a79a3d7a199c761dd370ff0fadac
darknesshaotran <darknessclmm@gmail.com>
2023-10-28 09:01:37 +0700

    rating middlewares

---
+++ src/middlewares/rating.middlewares.js
@@ -0,0 +1,99 @@
+const { checkSchema } = require('express-validator');
+const HTTP_STATUS = require('../constants/httpStatus');
+const ErrorsWithStatus = require('../constants/Error');
+const validate = require('../utils/validation');
+const db = require('../models');
+
+const checkRatingExistsValidator = checkSchema(
+    {
+        id_rating: {
+            custom: {
+                options: async (value) => {
+                    const rating = await db.Rating.findOne({
+                        where: {
+                            id: value,
+                        },
+                        attributes: {
+                            exclude: ['createdAt', 'updatedAt'],
+                        },
+                    });
+                    if (!rating) {
+                        throw new ErrorsWithStatus({ status: HTTP_STATUS.NOT_FOUND, message: 'rating not found' });
+                    }
+                    return true;
+                },
+            },
+        },
+    },
+    ['params'],
+);
+const checkAddRatingValidator = checkSchema(
+    {
+        id_shoes: {
+            custom: {
+                options: async (value) => {
+                    const shoes = await db.Shoes.findOne({
+                        where: {
+                            id: value,
+                        },
+                        attributes: {
+                            exclude: ['createdAt', 'updatedAt'],
+                        },
+                    });
+                    if (!shoes) {
+                        throw new ErrorsWithStatus({ status: HTTP_STATUS.NOT_FOUND, message: 'shoes not found' });
+                    }
+                    return true;
+                },
+            },
+        },
+        star: {
+            notEmpty: {
+                errorMessage: 'star is required',
+            },
+            isNumeric: {
+                errorMessage: 'star must be number',
+            },
+            custom: {
+                options: async (value) => {
+                    if (value < 1 || value > 5) {
+                        throw new ErrorsWithStatus({
+                            status: HTTP_STATUS.BAD_REQUEST,
+                            message: 'star must be from 1 to 5',
+                        });
+                    }
+                    return true;
+                },
+            },
+        },
+    },
+    ['body'],
+);
+const checkUpdateRatingValidator = checkSchema(
+    {
+        star: {
+            notEmpty: {
+                errorMessage: 'star is required',
+            },
+            isNumeric: {
+                errorMessage: 'star must be number',
+            },
+            custom: {
+                options: async (value) => {
+                    if (value < 1 || value > 5) {
+                        throw new ErrorsWithStatus({
+                            status: HTTP_STATUS.BAD_REQUEST,
+                            message: 'star must be from 1 to 5',
+                        });
+                    }
+                    return true;
+                },
+            },
+        },
+    },
+    ['body'],
+);
+
+exports.RatingExistsValidator = validate(checkRatingExistsValidator);
+exports.AddRatingValidator = validate(checkAddRatingValidator);
+exports.UpdateRatingValidator = validate(checkUpdateRatingValidator);
--- src/routes/rating.routes.js
+++ src/routes/rating.routes.js
@@ -4,8 +4,24 @@ const { accessTokenValidator, refreshTokenValidator } = require('../middlewares/
 const router = Router();
 
 const ratingControllers = require('../controllers/rating.controllers');
+const {
+    RatingExistsValidator,
+    AddRatingValidator,
+    UpdateRatingValidator,
+} = require('../middlewares/rating.middlewares');
 
-router.post('/add', accessTokenValidator, wrapController(ratingControllers.addRating));
-router.delete('/delete/:id_rating', accessTokenValidator, wrapController(ratingControllers.deleteRating));
-router.put('/update/:id_rating', accessTokenValidator, wrapController(ratingControllers.updateRating));
+router.post('/add', accessTokenValidator, AddRatingValidator, wrapController(ratingControllers.addRating));
+router.delete(
+    '/delete/:id_rating',
+    accessTokenValidator,
+    RatingExistsValidator,
+    wrapController(ratingControllers.deleteRating),
+);
+router.put(
+    '/update/:id_rating',
+    accessTokenValidator,
+    RatingExistsValidator,
+    UpdateRatingValidator,
+    wrapController(ratingControllers.updateRating),
+);
 module.exports = router;
