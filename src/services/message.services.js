12ac3c0f9c7c1f6cfa78d9b7038c8c2ee6c21719
darknesshaotran <darknessclmm@gmail.com>
2023-11-03 23:16:47 +0700

    update chatting

--- src/services/message.services.js
+++ src/services/message.services.js
@@ -4,9 +4,76 @@ const ErrorsWithStatus = require('../constants/Error');
 const HTTP_STATUS = require('../constants/httpStatus');
 
 class MessageServices {
-    async addMessage(star, comment, userID, id_shoes) {}
-    async getConversationList(userID) {}
-    async getConversationOfAnUser(userID, chat_user_ID) {}
+    async addMessage(id_reciever, id_sender, content) {
+        await db.Message.create({
+            id_reciever: id_reciever,
+            id_sender: id_sender,
+            content: content,
+        });
+        return {
+            success: true,
+            message: 'send message successfully',
+        };
+    }
+    async getConversationList(userID) {
+        const conversations = await db.Message.findAll({
+            where: {
+                [Op.or]: [
+                    {
+                        id_reciever: userID,
+                    },
+                    {
+                        id_sender: userID,
+                    },
+                ],
+            },
+            attributes: [
+                [db.sequelize.literal('DISTINCT `id_reciever`'), 'id'],
+                [db.sequelize.literal('MAX(`createdAt`)'), 'last_message_time'],
+            ],
+            include: [
+                {
+                    model: db.User,
+                    as: 'receiver',
+                    attributes: ['id', 'username', 'avatar'],
+                },
+                {
+                    model: db.User,
+                    as: 'sender',
+                    attributes: ['id', 'username', 'avatar'],
+                },
+            ],
+            group: ['id_reciever'],
+            order: [[db.sequelize.literal('last_message_time'), 'DESC']],
+        });
+        return {
+            success: true,
+            conversations: conversations,
+        };
+    }
+    async getConversationOfAnUser(userID, chat_user_ID, page, limit) {
+        const messages = await db.Message.findAll({
+            where: {
+                [Op.or]: [
+                    {
+                        id_reciever: userID,
+                        id_sender: chat_user_ID,
+                    },
+                    {
+                        id_reciever: chat_user_ID,
+                        id_sender: userID,
+                    },
+                ],
+            },
+            offset: (page - 1) * limit,
+            limit: limit,
+            order: [['createdAt', 'ASC']],
+        });
+        return {
+            success: true,
+            messages: messages,
+        };
+    }
 }
 
 module.exports = new MessageServices();
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,6 +4,7 @@ const { USERS_MESSAGES } = require('../constants/messages');
 const HTTP_STATUS = require('../constants/httpStatus');
 const db = require('../models');
 const { Op } = require('sequelize');
+const messageServices = require('../services/message.services');
 
 const initSocket = (httpServer) => {
     const io = new Server(httpServer, {
@@ -20,12 +21,13 @@ const initSocket = (httpServer) => {
             users[userID] = {
                 socket_id: socket.id,
             };
-            // server lắng nghe sự kiện gửi tin nhắn của socket A, gửi đến socket B sự kiện receive_message
-            socket.on('send_message', (data) => {
+            // server lắng nghe sự kiện send_message của socket A, gửi đến socket B sự kiện receive_message
+            socket.on('send_message', async (data) => {
                 const socket_id_receiver = users[data.id_receiver].socket_id;
                 if (socket_id_receiver) {
                     io.to(socket_id_receiver).emit('receive_message', { content: data.content, id_sender: userID });
                 }
+                await messageServices.addMessage(data.id_receiver, userID, data.content);
             });
         }
 
